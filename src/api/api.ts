3383f95f8fbf47a90f38cc233a7a4e9d44d7cc7e
magnes <szymon.magnuszewski0@gmail.com>
2022-08-29 23:30:06 +0200

    u

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,14 +2,13 @@ import { v4 as uuidv4 } from 'uuid';
 import { LocationApiData, LocationSearchData } from '../types/types';
 
 const key = process.env.REACT_APP_IPSTACK_API_KEY;
+const url = process.env.REACT_APP_IPSTACK_URL;
 
 export const apiCall = async (
   value: string | null,
   callback: (location: LocationSearchData) => void
 ) => {
-  const respone = await fetch(`/api/${value || 'check'}?access_key=${key}`, {
-    referrerPolicy: 'unsafe-url',
-  });
+  const respone = await fetch(`${url}${value || 'check'}?access_key=${key}`);
 
   const data: LocationApiData = await respone.json();
 
